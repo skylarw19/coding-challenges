a179119aa222c40ce6ee22dfa58de116b184b702
Skylar Wong <li.q.wong@gmail.com>
2020-09-02 19:27:34 -0500

    practicing recusrion with power func

--- challenges.js
+++ challenges.js
@@ -92,4 +92,19 @@ var isPalindrome = function(s) {
     return true;
 };
 
-console.log(isPalindrome("A man, a plan, a canal: Panama"))
+// console.log(isPalindrome("A man, a plan, a canal: Panama"))
+
+////power function. ex power(2,3) returns 8
+var power = function(n,e) {
+    let result = n;
+    for(let i=1; i<e; i++){
+        result = result * n;
+    } return result;
+}
+
+var recursivePow = function(n,e){
+    if (e === 0 ) return 1;
+    else return n * recursivePow(n,e-1)
+}
+
+console.log(recursivePow(2,3))
