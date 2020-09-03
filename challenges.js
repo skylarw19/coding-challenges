e81ee9d01810941f7dc282d9cbd8c2a08f1c9786
Skylar Wong <li.q.wong@gmail.com>
2020-09-03 18:29:47 -0500

    update leetcode reverse integer function

--- challenges.js
+++ challenges.js
@@ -109,13 +109,17 @@ var recursivePow = function(n,e){
 
 // console.log(recursivePow(2,3))
 
+//LEETCODE REVERSE INTEGER
+//Given a 32-bit signed integer, reverse digits of an integer
+// Assume we are dealing with an environment which could only store integers within the 32-bit signed integer 
+// range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 var reverse1 = function(x) {
     let str = x.toString().split('');
     let revNum;
     if (x > 0 || x===0 ) {
         revNum = parseInt(str.reverse().join(''));  
     }
-    else if (x<0) {
+    else {
         str.shift()
         revNum = parseInt(str.reverse().join(''))*-1; 
     }  
