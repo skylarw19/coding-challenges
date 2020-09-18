aafc4d7edf7c789d41c9b542c674f7194feff46e
Skylar Wong <li.q.wong@gmail.com>
2020-09-17 19:30:20 -0500

    leetcode 121 best time to buy sell stock

--- challenges.js
+++ challenges.js
@@ -166,9 +166,29 @@ var reverse1 = function(x) {
 var isPalNum = function(x) {
     if (x < 0) return false;
     let revX = parseInt(x.toString().split('').reverse().join(''));
-    console.log(revX)
     if (x === revX) return true;
     else return false
 };
 
 console.log(isPalNum(10))
+
+///leetcode 121 Best Time to Buy and Sell Stock
+var maxProfit = function(prices) {
+    // let maxProfit=0;
+    // for(let i=0; i<prices.length-1; i++){
+    //     for(let j=i+1; j<prices.length; j++){
+    //         if (prices[j]-prices[i] > maxProfit) {
+    //             maxProfit = prices[j] - prices[i];
+    //         }
+    //     }
+    // } return maxProfit;
+    //^ this is O(N^2)
+    
+    // for faster runtime O(N)
+    let maxProfit = 0;
+    let low=prices[0];
+    for(let i=0; i<prices.length; i++){
+        if (prices[i] < low) low = prices[i]
+        else if (prices[i]-low > maxProfit) maxProfit = prices[i]-low;
+    } return maxProfit;
+};
