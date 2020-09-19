6acbc2c81354c87c7e0728ca02d76313a634dcb3
Skylar Wong <li.q.wong@gmail.com>
2020-09-18 22:44:09 -0500

    leetcode 206. reverse singly linked list

---
+++ reverseLinkedList.js
@@ -0,0 +1,24 @@
+/** leetcode 206. reverse linked list
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let current = head;
+    if(!current) return current;
+    let newCur = head.next;
+    current.next = null;
+    while(newCur){
+        let temp = newCur.next;
+        newCur.next = current;
+        current = newCur;
+        newCur = temp;
+    }
+    return current;
+};
