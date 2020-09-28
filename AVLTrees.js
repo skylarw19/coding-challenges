d19202df57c35548f5d3e270216822bfe79eae78
Skylar Wong <li.q.wong@gmail.com>
2020-09-27 22:05:52 -0500

    AVLTree.js - function to rotate right to balance tree

---
+++ AVLTrees.js
@@ -0,0 +1,43 @@
+// AVL tree = self balance tree. 
+// code from youtube: https://www.youtube.com/watch?v=rbg7Qf8GkQ4&&ab_channel=TusharRoy-CodingMadeSimple
+
+class BinaryNode {
+    constructor(data){
+      this.data = data;
+      this.left = null;
+      this.right = null;
+    }
+}
+
+class AVLTree {
+    constructor(){
+        this.root = null;
+    }
+
+    //PSUEDO CODE
+    rotateRight(node){
+        let newRoot = this.left;    //set newRoot to node 15
+        this.root.left = newRoot.right; // this.root is still 20. set left to 18
+        newRoot.right = this.root; //set right of newRoot to branch of 20. the left side is already set.
+        // set new heights after rotation
+        this.root.height = max(root.left, root.right)+1;   //take max heigh of left or right sides and add 1
+        newRoot.height= max(newRoot.left, newRoot.right)+1;
+        return newRoot;
+    }
+}
+
+// think of a tree that looks like this:
+//             20
+//            /  \
+//         15    25  
+//         / \
+//       10   18
+//     /
+//    2
+// the tree is unbalanced at node 20 b/c height of 3 on left, 1 on right. difference = 2 > 1 -> unbalanced
+// so this is left left unbalanced, solution is to rotate right so that 15 becomes the root node:
+//             15
+//            /  \
+//         10    20  
+//         /    /  \
+//       2    18   25 
