d66f6f47541f8bbe5454513ebd40f269b6bee9ec
Skylar Wong <li.q.wong@gmail.com>
2021-02-09 14:47:16 -0600

    code challenge for better.com

---
+++ better.js
@@ -0,0 +1,110 @@
+//days of the wk
+//function receives a string S, and you wanna find the day it is K days later
+function daysOfWk (S,K) {
+    let startDay;
+    switch (S) {
+        case "Mon": startDay = 1; break;
+        case "Tue": startDay = 2; break;
+        case "Wed": startDay = 3; break;
+        case "Thu": startDay = 4; break;
+        case "Fri": startDay = 5; break;
+        case "Sat": startDay = 6; break;
+        case "Sun": startDay = 7; break;
+    }
+    
+    let endDay = (startDay+K)%7;
+    switch (endDay) {
+        case 1: return "Mon";
+        case 2: return "Tue";
+        case 3: return "Wed"
+        case 4: return "Thu"
+        case 5: return "Fri"
+        case 6: return "Sat"
+        case 0: return "Sun"
+    }
+    
+}
+
+// console.log(daysOfWk("Mon", 13))
+
+function insert5 (N){
+    stringN = N.toString(); //convert num to str
+    if (N < 0){ //if negative number
+        let str = stringN.slice(1)
+
+        let i=0;  
+        while (str[i] < 5){ // less than 5 for neg nums
+            i++;
+        }
+    
+        let arr = str.split('')
+        arr.splice(i,0,"5")  //insert 5
+        arr.splice(0,0,"-")
+        let joinedArr = arr.join("")
+        let parsed = parseInt(joinedArr)  //convert string to num
+    
+        return parsed;
+
+    } else { // if positive number
+        let str = stringN;
+
+        // find index
+        let i=0;
+        while (str[i] > 5){ 
+            i++;
+        }
+
+        //insert 5
+        let arr = str.split('')
+        arr.splice(i,0,"5")  
+        let joinedArr = arr.join("")
+        let parsed = parseInt(joinedArr)  //convert string to num
+    
+        return parsed;
+    }
+}
+
+// console.log(insert5(268))
+// console.log(insert5(670))
+// console.log(insert5(0))
+// console.log(insert5(-999))
+
+
+//didn't finish
+function balanced(S){
+    let upperSet = {}
+    let lowerSet = {}
+    let count = 0;
+    let minlength = 0;
+    let arr = S.split("")
+    let balance = false;
+
+    arr.forEach((el) => {
+        if (el === el.toUpperCase()){
+            if (!upperSet[el]){
+                upperSet[el] = 1;
+                if (lowerSet[el.toLowerCase()]){
+                    balance = true;
+                }
+            } 
+        } else {
+            lowerSet[el] = 1;
+            if (upperSet[el.toUpperCase()]){
+                balance = true
+            }
+        }
+        count++;
+        console.log(`letter: ${el}`)
+        console.log(upperSet)
+        console.log(lowerSet)
+        console.log(`count: ${count}`)
+        console.log(`balance: ${balance}`)
+        
+        if (balance) {
+            console.log("balance")
+        } else {console.log("no")}
+    })
+   
+}
+
+// console.log(balanced("CATatc"))
